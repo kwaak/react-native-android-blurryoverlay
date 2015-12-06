f0498650b1f673d4c7108b7bd5f4cc6b56e3cf14
kwaak <taco.perquin@gmail.com>
2015-12-06 17:15:34 +0100

    Add View proptypes Upgrade React Native to 0.16

--- index.android.js
+++ index.android.js
@@ -1,8 +1,9 @@
-var { requireNativeComponent, PropTypes } = require('react-native');
+var { requireNativeComponent, PropTypes, View } = require('react-native');
 
 var iface = {
     name: 'BlurryOverlay',
     propTypes: {
++        ...View.propTypes,
         radius: PropTypes.number,
         sampling: PropTypes.number,
         color: PropTypes.string,
